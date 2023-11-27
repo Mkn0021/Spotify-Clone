6faf87accd2ef9258e03436a022fbbf24e06ee44
M. A. Masud Karim <nayeem21122002@gmail.com>
2023-11-27 14:08:23 +0600

    Playlist header color is now Dynamic -also bring back the 2nd title

--- lib/CustomWidgets/bouncy_playlist_header_scroll_view.dart
+++ lib/CustomWidgets/bouncy_playlist_header_scroll_view.dart
@@ -5,6 +5,8 @@ import 'dart:io';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:spotify/CustomWidgets/svg_button.dart';
+import 'package:spotify/Helpers/dominant_color.dart';
+//import 'package:spotify/Helpers/dominant_color.dart';
 
 class BouncyPlaylistHeaderScrollView extends StatelessWidget {
   final ScrollController scrollController;
@@ -41,32 +43,47 @@ class BouncyPlaylistHeaderScrollView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Widget image = imageUrl == null
-        ? Image(
+    Widget image = Image(
+      fit: BoxFit.cover,
+      image: AssetImage(placeholderImage),
+    );
+
+    Color? gradientColor;
+
+    Future<void> processImage() async {
+      if (localImage) {
+        image = Image(
+          fit: BoxFit.cover,
+          image: FileImage(
+            File(imageUrl!),
+          ),
+        );
+        final List<Color> colors = await getColors(
+          imageProvider: FileImage(
+            File(imageUrl!),
+          ),
+        );
+        gradientColor = colors.isNotEmpty == true ? colors[0] : null;
+      } else {
+        image = CachedNetworkImage(
+          fit: BoxFit.cover,
+          errorWidget: (context, _, __) => Image(
             fit: BoxFit.cover,
             image: AssetImage(placeholderImage),
-          )
-        : localImage
-            ? Image(
-                image: FileImage(
-                  File(
-                    imageUrl!,
-                  ),
-                ),
-                fit: BoxFit.cover,
-              )
-            : CachedNetworkImage(
-                fit: BoxFit.cover,
-                errorWidget: (context, _, __) => Image(
-                  fit: BoxFit.cover,
-                  image: AssetImage(placeholderImage),
-                ),
-                imageUrl: imageUrl!,
-                placeholder: (context, url) => Image(
-                  fit: BoxFit.cover,
-                  image: AssetImage(placeholderImage),
-                ),
-              );
+          ),
+          imageUrl: imageUrl!,
+          placeholder: (context, url) => Image(
+            fit: BoxFit.cover,
+            image: AssetImage(placeholderImage),
+          ),
+        );
+        final List<Color> colors = await getColors(
+          imageProvider: CachedNetworkImageProvider(imageUrl!),
+        );
+        gradientColor = colors.isNotEmpty == true ? colors[0] : null;
+      }
+    }
+
     final bool rotated =
         MediaQuery.of(context).size.height < MediaQuery.of(context).size.width;
     final double expandedHeight = rotated
@@ -74,131 +91,156 @@ class BouncyPlaylistHeaderScrollView extends StatelessWidget {
         : MediaQuery.of(context).size.width;
     final double screenWidth = MediaQuery.of(context).size.width;
 
-    return CustomScrollView(
-      controller: scrollController,
-      shrinkWrap: shrinkWrap,
-      physics: const BouncingScrollPhysics(),
-      slivers: [
-        AnimatedBuilder(
-          animation: scrollController,
-          child: FlexibleSpaceBar(
-            background: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Colors.blueGrey,
-                    Colors.black,
-                  ],
-                  stops: [0.0, 0.8],
-                ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Stack(
-                    children: [
-                      SizedBox(
-                        width: screenWidth / 1.9,
-                        child: Card(
-                          elevation: 1,
-                          clipBehavior: Clip.antiAlias,
-                          child: image,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
+    return FutureBuilder<void>(
+      future: processImage(),
+      builder: (context, snapshot) {
+        return CustomScrollView(
+          controller: scrollController,
+          shrinkWrap: shrinkWrap,
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            AnimatedBuilder(
+              animation: scrollController,
+              child: FlexibleSpaceBar(
+                background: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        gradientColor ?? Colors.transparent,
+                        Colors.black,
+                      ],
+                      stops: const [0.0, 0.8],
                     ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
                   ),
-                  if (secondarySubtitle != null &&
-                      secondarySubtitle!.isNotEmpty)
-                    Text(
-                      secondarySubtitle!,
-                      softWrap: false,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: Theme.of(context).textTheme.bodySmall!.color,
-                      ),
-                    ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Row(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       const SizedBox(
-                        width: 5,
+                        height: 40,
                       ),
-                      ...actions ?? [], // Use the null-aware spread operator
-                      const Spacer(),
-                      if (onShuffleTap != null)
-                        IconButton(
-                          onPressed: () {
-                            onShuffleTap!.call();
-                          },
-                          icon: const Icon(Icons.shuffle_rounded),
+                      Stack(
+                        children: [
+                          SizedBox(
+                            width: screenWidth / 1.9,
+                            child: Card(
+                              elevation: 1,
+                              clipBehavior: Clip.antiAlias,
+                              child: image,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        title,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
                         ),
-                      if (onPlayTap != null)
-                        Transform.scale(
-                          scale: 1.5,
-                          child: SvgIconButton(
-                            selectedSVG: 'assets/play_round.svg',
-                            unselectedSVG: 'assets/pause_round.svg',
-                            selectedColor: buttonColor ??
-                                Theme.of(context).colorScheme.secondary,
-                            unselectedColor: buttonColor ??
-                                Theme.of(context).colorScheme.secondary,
-                            iconSize: 40,
-                            onTap: () {
-                              onPlayTap!.call();
-                            },
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      if (subtitle != null && subtitle!.isNotEmpty)
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            bottom: 3.0,
+                          ),
+                          child: Text(
+                            subtitle!,
+                            softWrap: false,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 3,
+                            style: TextStyle(
+                              fontSize: 12,
+                              color:
+                                  Theme.of(context).textTheme.bodySmall!.color,
+                            ),
+                          ),
+                        ),
+                      if (secondarySubtitle != null &&
+                          secondarySubtitle!.isNotEmpty)
+                        Text(
+                          secondarySubtitle!,
+                          softWrap: false,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Theme.of(context).textTheme.bodySmall!.color,
                           ),
                         ),
                       const SizedBox(
-                        width: 20,
+                        height: 15,
+                      ),
+                      Row(
+                        children: [
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          ...actions ??
+                              [], // Use the null-aware spread operator
+                          const Spacer(),
+                          if (onShuffleTap != null)
+                            IconButton(
+                              onPressed: () {
+                                onShuffleTap!.call();
+                              },
+                              icon: const Icon(Icons.shuffle_rounded),
+                            ),
+                          if (onPlayTap != null)
+                            Transform.scale(
+                              scale: 1.5,
+                              child: SvgIconButton(
+                                selectedSVG: 'assets/play_round.svg',
+                                unselectedSVG: 'assets/pause_round.svg',
+                                selectedColor: buttonColor ??
+                                    Theme.of(context).colorScheme.secondary,
+                                unselectedColor: buttonColor ??
+                                    Theme.of(context).colorScheme.secondary,
+                                iconSize: 40,
+                                onTap: () {
+                                  onPlayTap!.call();
+                                },
+                              ),
+                            ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
+              builder: (context, child) {
+                if (scrollController.offset.roundToDouble() >
+                    expandedHeight - 80) {
+                  isTransparent.value = false;
+                } else {
+                  isTransparent.value = true;
+                }
+                return SliverAppBar(
+                  elevation: 0,
+                  stretch: true,
+                  pinned: true,
+                  centerTitle: true,
+                  //floating: true,
+                  backgroundColor:
+                      isTransparent.value ? Colors.transparent : null,
+                  iconTheme: Theme.of(context).iconTheme,
+                  expandedHeight: expandedHeight - 20,
+                  flexibleSpace: child,
+                );
+              },
             ),
-          ),
-          builder: (context, child) {
-            if (scrollController.offset.roundToDouble() > expandedHeight - 80) {
-              isTransparent.value = false;
-            } else {
-              isTransparent.value = true;
-            }
-            return SliverAppBar(
-              elevation: 0,
-              stretch: true,
-              pinned: true,
-              centerTitle: true,
-              //floating: true,
-              backgroundColor: isTransparent.value ? Colors.transparent : null,
-              iconTheme: Theme.of(context).iconTheme,
-              expandedHeight: expandedHeight - 20,
-              flexibleSpace: child,
-            );
-          },
-        ),
-        sliverList,
-      ],
+            sliverList,
+          ],
+        );
+      },
     );
   }
 }
